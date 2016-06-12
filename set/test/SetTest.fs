b512b578e35331fa5deb0c41e163a3430f8760f2
Al Scott <al.scott@atomicobject.com>
2016-06-12 12:06:38 -0400

    Added explicit test for count.

--- set/test/SetTest.fs
+++ set/test/SetTest.fs
@@ -10,7 +10,7 @@ type ``#Put`` ()=
   let testSet = new CustomSet<int>()
 
   [<Test>]
-  member x.``does cause error when putting the same key twice`` ()=
+  member x.``does not cause error when putting the same key twice`` ()=
     testSet.Put(5)
     testSet.Put(5)
     testSet.Count |> should equal 1
@@ -30,15 +30,31 @@ type ``#Remove`` ()=
     testSet.Remove(3, -1) |> should equal -1
 
 [<TestFixture>]
+type ``#Count`` ()=
+  let testSet = new CustomSet<int>()
+
+  [<Test>]
+  member x.``returns the number of items in the set`` ()=
+    testSet.Count |> should equal 0
+    testSet.Put(5)
+    testSet.Count |> should equal 1
+    testSet.Put(10)
+    testSet.Count |> should equal 2
+    testSet.Put(5)
+    testSet.Count |> should equal 2
+    ignore(testSet.Remove(5, 0))
+    testSet.Count |> should equal 1
+
+[<TestFixture>]
 type ``#Empty`` ()=
   let testSet = new CustomSet<string>()
 
   [<Test>]
-  member x.``set is empty when first created`` ()=
+  member x.``is true when first created`` ()=
     testSet.Empty |> should be True
 
   [<Test>]
-  member x.``set changes emptiness status as items are added and removed`` ()=
+  member x.``changes status as items are added and removed`` ()=
     testSet.Put("Hello")
     testSet.Empty |> should be False
     ignore(testSet.Remove("Hello", ""))
